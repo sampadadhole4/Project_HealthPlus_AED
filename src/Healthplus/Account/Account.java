e55dd742c343cbb34e4583a697f5548b3bb8a2a6
sampadadhole4 <dhole.s@northeastern.edu>
2022-05-01 21:27:45 -0400

    added Account and Applicant directory

---
+++ src/Healthplus/Account/Account.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Healthplus.Account;
+
+import Healthplus.FundRaiser.FundRaiser;
+
+public class Account {
+        private FundRaiser agency;
+
+    public FundRaiser getAgency() {
+        return agency;
+    }
+
+    public void setAgency(FundRaiser agency) {
+        this.agency = agency;
+    }
+}
