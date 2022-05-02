27737f2f1651472c2c4039d10c5da8b61d5e702e
sampadadhole4 <dhole.s@northeastern.edu>
2022-05-02 01:09:55 -0400

    modified Account

--- src/Healthplus/Account/Account.java
+++ src/Healthplus/Account/Account.java
@@ -12,8 +12,9 @@ public class Account {
 
     public FundRaiser getAgency() {
         return agency;
+        
     }
-    
+
 
     public void setAgency(FundRaiser agency) {
         this.agency = agency;
