53869fee5c970503a4de81ec687fe0b706f04320
sampadadhole4 <dhole.s@northeastern.edu>
2022-05-01 21:56:21 -0400

    added emeregcyservices

---
+++ src/Healthplus/EmergencyServices/Emergency.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Healthplus.EmergencyServices;
+
+/**
+ *
+ * @author sampadadhole
+ */
+public class Emergency {
+    private String Country;
+    private String City;
+    private String State;
+    private String drivername;
+
+    public String getDrivername() {
+        return drivername;
+    }
+
+    public void setDrivername(String drivername) {
+        this.drivername = drivername;
+    }
+    private int Eid;
+    private static int count = 1;
+    
+     public Emergency() {
+        Eid = count;
+        count++;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public int getEid() {
+        return Eid;
+    }
+
+    public void setEid(int Eid) {
+        this.Eid = Eid;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Emergency.count = count;
+    }
+
+   
+//    @Override
+//    public String toString() {
+//        return country;
+//    }
+
+}
